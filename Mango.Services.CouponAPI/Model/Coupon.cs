5353efb9910fba3480ae7857b1fc1ef5b0ecb162
Abhijeet44 <abhibirajdar8585@gmail.com>
2025-12-22 10:31:30 +0530

    Build first coupon api service

---
+++ Mango.Services.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Model
+{
+	public class Coupon
+	{
+		public int CouponId { get; set; }
+		public string CouponCode { get; set; }	
+		public double DiscountAmount { get; set; }
+		public int MinAmount { get; set; }
+
+	}
+}
