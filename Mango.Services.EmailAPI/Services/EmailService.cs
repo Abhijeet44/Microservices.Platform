3f8d557958f7fa64cf22bebd160b3d729ce3a667
Abhijeet44 <abhibirajdar8585@gmail.com>
2026-01-30 23:17:52 +0530

    Registerd service bus to consume email api service

---
+++ Mango.Services.EmailAPI/Services/EmailService.cs
@@ -0,0 +1,60 @@
+﻿using Mango.Services.EmailAPI.Data;
+using Mango.Services.EmailAPI.Model;
+using Mango.Services.EmailAPI.Models.Dtos;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Mango.Services.EmailAPI.Services
+{
+	public class EmailService : IEmailService
+	{
+		private DbContextOptions<AppDbContext> options;
+
+		public EmailService(DbContextOptions<AppDbContext> options)
+		{
+			this.options = options;
+		}
+
+		public async Task EmailCartAndLog(CartDto cartDto)
+		{
+			StringBuilder message = new StringBuilder();
+			message.AppendLine("<br>New Cart Created ");
+			message.AppendLine("<br> Total :" + cartDto.CartHeader.CartTotal);
+			message.AppendLine("<br>");
+			message.AppendLine("<ul/>");
+			foreach(var item in cartDto.CartDetails)
+			{
+				message.AppendLine("<li>");
+		        message.Append(item.Product.Name + " X " + item.Count);
+				message.Append("</li>");
+			}
+			message.AppendLine("<ul/>");
+
+			await LogAndEmail(message.ToString(), cartDto.CartHeader.Email);
+
+		}
+
+		private async Task<bool> LogAndEmail(string message, string email)
+		{
+			try
+			{
+				EmailLogger emailLogger = new EmailLogger()
+				{
+					Email = email,
+					EmailSent = DateTime.Now,
+					Message = message
+				};
+
+				await using var _db = new AppDbContext(options);
+				await _db.EmailLoggers.AddAsync(emailLogger);
+				await _db.SaveChangesAsync();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+	}
+}
