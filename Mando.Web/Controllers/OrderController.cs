c504b6169ff1ecdb1dc0196a1016ff05716ae801
Abhijeet44 <abhibirajdar8585@gmail.com>
2026-03-05 21:51:10 +0530

    Manage order as per the user role

---
+++ Mando.Web/Controllers/OrderController.cs
@@ -0,0 +1,114 @@
+﻿using Mango.Web.Model;
+using Mango.Web.Models;
+using Mango.Web.Services.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Mango.Web.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IOrderService _orderService;
+
+		public OrderController(IOrderService orderService)
+		{
+			_orderService = orderService;
+		}
+
+		public IActionResult OrderIndex()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> OrderDetail(int orderId)
+		{
+			OrderHeaderDto orderHeaderDto = new OrderHeaderDto();
+			string userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+			var response = await _orderService.GetOrder(orderId);
+			if (response != null && response.isSuccess)
+			{
+				orderHeaderDto = JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+			}
+			if (!User.IsInRole(SD.RoleAdmin) && userId != orderHeaderDto.UserId)
+			{
+				return NotFound();
+			}
+
+			return View(orderHeaderDto);
+		}
+
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeaderDto> list;
+			string userId = "";
+			if (!User.IsInRole(SD.RoleAdmin))
+			{
+				userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+			}
+			ResponseDto response =  _orderService.GetAllOrders(userId).GetAwaiter().GetResult();
+			if(response != null && response.isSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+				switch (status)
+				{
+					case "approved":
+						list = list.Where(u => u.Status == SD.Status_Pending);
+						break;
+					case "readyforpickup":
+						list = list.Where(u => u.Status == SD.Status_Pending);
+						break;
+					case "cancelled":
+						list = list.Where(u => u.Status == SD.Status_Pending);
+						break;
+					default:
+						break;
+				}
+			}
+			else
+			{
+				list = new List<OrderHeaderDto>();
+			}
+			return Json(new { data = list });
+		}
+
+		[HttpPost("OrderReadyForPickUp")]
+		public async Task<IActionResult> OrderReadyForPickUp(int orderId)
+		{
+			var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_ReadyForPickup);
+			if (response != null && response.isSuccess)
+			{
+				TempData["success"] = "Status Updated Successfully.";
+				return RedirectToAction(nameof(OrderIndex), new {orderId = orderId});
+			}
+			return View(orderId);
+		}
+
+		[HttpPost("CompleteOrder")]
+		public async Task<IActionResult> CompleteOrder(int orderId)
+		{
+			var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Complete);
+			if (response != null && response.isSuccess)
+			{
+				TempData["success"] = "Status Updated Successfully.";
+				return RedirectToAction(nameof(OrderIndex), new { orderId = orderId });
+			}
+			return View(orderId);
+		}
+
+		[HttpPost("CancleOrder")]
+		public async Task<IActionResult> CancleOrder(int orderId)
+		{
+			var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Cancelled);
+			if (response != null && response.isSuccess)
+			{
+				TempData["success"] = "Status Updated Successfully.";
+				return RedirectToAction(nameof(OrderIndex), new { orderId = orderId });
+			}
+			return View(orderId);
+		}
+	}
+}
--- Mando.Web/Services/IService/IOrderService.cs
+++ Mando.Web/Services/IService/IOrderService.cs
@@ -8,5 +8,8 @@ namespace Mango.Web.Services.IService
 		Task<ResponseDto> CreateOrder(CartDto cartDto);
 		Task<ResponseDto> CreateStripeSession(StripeRequestDto stripeRequestDto);
 		Task<ResponseDto> ValidateStripeSession(int orderHeaderId);
+		Task<ResponseDto> GetAllOrders(string? userId);
+		Task<ResponseDto> GetOrder(int id);
+		Task<ResponseDto> UpdateOrderStatus(int orderId, string newStatus);
 	}
 }
