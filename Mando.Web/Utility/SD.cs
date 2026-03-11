2e6ce79e9f06c7a4f6560012f79942decb5d9139
Abhijeet44 <abhibirajdar8585@gmail.com>
2026-03-11 23:15:03 +0530

    implement gateway with ocelot

--- Mando.Web/Utility/SD.cs
+++ Mando.Web/Utility/SD.cs
@@ -8,8 +8,8 @@
 		public static String ShoppingCartAPIBase { get; set; }
 		public static string OrderAPIBase { get; set; }
 
-		public const string RoleAdmin = "ADMIN";
-		public const string RoleCustomer = "CUSTOMER";
+		public const string RoleAdmin = "Admin";
+		public const string RoleCustomer = "Customer";
 		public const string TokenCookie = "JwtToken";
 
 		public enum ApiType
